cf688f882a83e41ee6cfbc960a5e94219237b881
Faisal-pixel <adamsfaisal2003@gmail.com>
2023-01-16 12:24:53 +0100

    Completed the project, added search functionality with the use of pexels api, made it responsive also

---
+++ app.js
@@ -0,0 +1,80 @@
+const auth = "563492ad6f91700001000001a6945ff778ba4baeae419ca382fd7787";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const searchForm = document.querySelector(".search-form");
+const more = document.querySelector(".more");
+let page = 1;
+let searchValue;
+let fetchLink;
+let currentSearch;
+
+searchInput.addEventListener('input', updateInput)
+searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+    currentSearch = searchValue;
+    searchPhotos(searchValue);
+})
+more.addEventListener("click", loadMore);
+
+
+function updateInput(e) {
+    searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+    const dataFetch = await fetch(url, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: auth
+        }
+    })
+
+    return await dataFetch.json()
+}
+
+function generatePictures (data) {
+    data.photos.forEach(photo => {
+        
+        const galleryImg = document.createElement("div");
+        galleryImg.classList.add("gallery-img");
+        galleryImg.innerHTML = `
+        <div class="gallery-info">
+            <p>${photo.photographer}</p>
+            <a href=${photo.src.original}>Download</a>
+        </div>
+        <img src=${photo.src.large}> </img>
+        `
+        gallery.appendChild(galleryImg)
+    })
+}
+
+async function curatedPhotos() {
+    fetchLink = "https://api.pexels.com/v1/curated?per_page=15";
+    const data = await fetchApi(fetchLink)
+    generatePictures(data);
+}
+
+async function searchPhotos(query) {
+    clear();
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+    const data = await fetchApi(fetchLink)
+    generatePictures(data)
+}
+
+function clear() {
+    gallery.innerHTML = "";
+}
+
+async function loadMore() {
+    page++;
+    if(currentSearch) {
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`
+    } else {
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+    }
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
+
+curatedPhotos();
